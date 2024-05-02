72cfa3eed86771dbbd33f940b77763e3547d311a
Cherar <lareugerardo@gmail.com>
2024-05-02 19:50:35 -0300

    other: solucion en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import './App.css'
-// import off from './assets/off.png'
+import off from './assets/off.png'
 import on from './assets/on.png'
 import luzOn from './assets/luz-on.png'
-// import luzOff from './assets/luz-off.png'
+import luzOff from './assets/luz-off.png'
 import {useEffect, useState} from "react";
 import {getLuzStatus} from "./service/Api.js";
-// import {Spinner} from "./components/spinner";
+import {Spinner} from "./components/spinner.jsx";
 
 const App = () => {
     const [data, setData] = useState()
+    const [prendido, setPrendido] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         getLuzStatus(123)
             .then(response => {
-                setData(response)
+                setData(response.luz)
                 console.log('success', response)
             })
             .catch(error => {
+                setIsError(true)
                 console.log('error: ', error)
+            })
+            .finally(() => {
+                setLoading(false)
             })
     }, []);
 
+    if (isError) return <div>Error</div>
+
+    if (loading) return <Spinner />
+    const handleClick = () => {
+        setPrendido(x => !x)
+    }
+
   return (
     <div className="luz-layout">
-      <img className="luz-on" src={luzOn} />
-      {/*<img className="luz-off" src={luzOff} />*/}
-      <img className="on" src={on} />
-      {/*<img className="off" src={off} />*/}
-        {/*<Spinner />*/}
+      {data && prendido ? <img className="luz-on" src={luzOn} /> : <img className="luz-off" src={luzOff} />}
+      <img className="on" src={prendido ? on : off} onClick={handleClick} />
     </div>
   )
 }
--- src/service/Api.js
+++ src/service/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const get = (url) => axios.get(url)
     .then(req => req.data)
-    .catch(error => error.response.data)
+    // .catch(error => error.response.data)
 
 export const getLuzStatus = (userID) => get(`http://localhost:3000/segba/${userID}`)
